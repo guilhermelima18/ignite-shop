5c57a4f11bac97bb767f317be718d14697f917b6
guilhermelima18 <guilhermelima18@hotmail.com>
2022-11-20 16:55:10 -0300

    feat: configurando a lib stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
+import { styled } from "../styles";
+
+const Button = styled("button", {
+  backgroundColor: "$rocketseat",
+  border: 0,
+  color: "white",
+});
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return <Button>Enviar</Button>;
 }
